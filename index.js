05fe768c4181e1827a90fc4089d389bfe21cac01
tuckl <tuckerlarrabee13@gmail.com>
2022-03-05 16:07:34 -0500

    almost done

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-const inquirer = require('inquirer');
 const mysql2 = require('mysql2');
-
-
 
 const db = mysql2.createConnection(
     {
@@ -12,24 +9,5 @@ const db = mysql2.createConnection(
     },
     console.log('Connected to the tracker database')
 )
-
-
-// db.query(sql, params, (err, result) => {
-//     if (err) {
-//         console.log(err)
-//     }
-// })
-
-// db = util.promisify(db.query)
-
-// const main = async () => {
-//     const viewAllDept = await inquirer.prompt()
-//     if (viewAllDept) {
-//         db.query()
-//         main()
-//     }
-//     const answers2 = await inquirer.prompt()
-//     const result = await db.query()
-// }
 
 module.exports = db
--- utils/prompt.js
+++ utils/prompt.js
@@ -15,7 +15,9 @@ const main = async () => {
                 console.log(err);
                 return;
             }
+            console.log("========================")
             console.table(rows);
+            console.log("========================")
             main();
         })
     }
@@ -27,33 +29,26 @@ const main = async () => {
                 console.log(err);
                 return;
             }
+            console.log("========================")
             console.table(rows);
+            console.log("========================")
             main();
         })
     }
     if (viewAllDept.start === 'view all employees') {
-        //need to figure out how to do inner join to output manager names instead of manager id #
-        // SELECT emp.id, emp.first_name, emp.last_name, role.title job_title, department.name department, role.salary, CONCAT(emp.first_name, ' ', emp.last_name) AS Manager
-        //             FROM
-        //         employee emp
-        //             INNER JOIN
-        //         role ON emp.role_id = role.id
-        //             INNER JOIN
-        //         department ON role.department_id = department.id
-        //     ;
-
-        // "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;"
-
-        db.query((`SELECT employee.id AS employee_id, employee.first_name, employee.last_name, role.title AS job_title, department.name AS department, role.salary, CONCAT(employee.first_name, ' ', employee.last_name) AS Manager
-                    FROM employee
-                    INNER JOIN role on employee.role_id=role.id 
-                    INNER JOIN department ON role.department_id=department.id
+                db.query((`SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager 
+                    FROM employee 
+                    LEFT JOIN role on employee.role_id = role.id 
+                    LEFT JOIN department on role.department_id = department.id 
+                    LEFT JOIN employee manager on manager.id = employee.manager_id;
                     `), (err, rows) => {
             if (err) {
                 console.log(err);
                 return;
             }
+            console.log("========================")
             console.table(rows);
+            console.log("========================")
             main();
         })
     }
@@ -77,7 +72,9 @@ const main = async () => {
             if (err) {
                 console.log(err)
             }
+            console.log("========================")
             console.log(result)
+            console.log("========================")
             main();
         })
     }
@@ -116,31 +113,24 @@ const main = async () => {
         newRole.salary = salary.salary;
         const dept = await inquirer.prompt(
             {
-                type: 'input',
+                type: 'list',
                 name: 'department',
-                message: 'Please enter the department for this role',
-                validate: dept => {
-                    if (dept) {
-                        return true;
-                    } else {
-                        console.log('Please enter the department for this role')
-                    }
-                }
-
-            }
-        )
-        // figure out how to add the department_id properly? department name will already exist
-        newRole.department = dept.department;
+                message: 'Please select the department this role corresponds to',
+                choices: ["Sales", "Finance", "Engineering", "Legal"]
+            })
+            if (dept.department === 'Sales') {
+                newRole.department = 1
+            };
+            if (dept.department === 'Finance') {
+                newRole.department = 2
+            };
+            if (dept.department === 'Engineering') {
+                newRole.department = 3
+            };
+            if (dept.department === 'Legal') {
+                newRole.department = 4
+            };
         console.log(newRole)
-        db.query(`INSERT INTO department (name)
-                    VALUES ("${newRole.department})`, (err, result) => {
-            if (err) {
-                console.log(err)
-            } else {
-                console.log(result)
-            }
-        }
-        )
         db.query(`INSERT INTO role (title, salary, department_id)
                     VALUES ("${newRole.roleTitle}", "${newRole.salary}", "${newRole.department}")`, (err, result) => {
             if (err) {
@@ -152,9 +142,103 @@ const main = async () => {
     }
     //add new employee
     if (viewAllDept.start === 'add an employee') {
+        const newEmploy = {}
+        const addEmployeeFirst = await inquirer.prompt(
+            {
+                type: 'input',
+                name: 'firstName',
+                message: 'Please enter the first name of the employee you would like to add',
+                validate: firstName => {
+                    if (firstName) {
+                        return true;
+                    } else {
+                        console.log('Please enter the first name of the employee you would like to add')
+                    }
+                }
+            })
+        newEmploy.firstName = addEmployeeFirst.firstName;
+        const addEmployeeLast = await inquirer.prompt(
+            {
+                type: 'input',
+                name: 'lastName',
+                message: 'Please enter the last name of the employee you would like to add',
+                validate: lastName => {
+                    if (lastName) {
+                        return true;
+                    } else {
+                        console.log('Please enter the last name of the employee you would like to add')
+                    }
+                }
+            })
+        newEmploy.lastName = addEmployeeLast.lastName
+        const addEmployeeRole = await inquirer.prompt(
+            {
+               type: 'list',
+               name: 'role',
+               message: "Please select the role of the employee you would like to add",
+               choices: ["Salesperson", "Accountant", "Software Engineer", "Lawyer"]
+            })
+                if (addEmployeeRole.role === 'Salesperson') {
+                    newEmploy.role = 1
+                };
+                if (addEmployeeRole.role === 'Accountant') {
+                    newEmploy.role = 2
+                };
+                if (addEmployeeRole.role === 'Software Engineer') {
+                    newEmploy.role = 3
+                };
+                if (addEmployeeRole.role === 'Lawyer') {
+                    newEmploy.role = 4
+                };
+        const addEmployeeManager = await inquirer.prompt(
+            {
+               type: 'list',
+               name: 'manager',
+               message: "Please select the manager of the employee you would like to add",
+               choices: ["Bob Johnson", "Mike Chan", "No manager"]
+            })
+            if (addEmployeeManager.manager === 'Bob Johnson') {
+                newEmploy.manager = 1
+            };
+            if (addEmployeeManager.manager === 'Mike Chan') {
+                newEmploy.manager = 2
+            };
+            if (addEmployeeManager.manager === 'No manager') {
+                newEmploy.manager = null
+            };
+            console.log(newEmploy) 
+
+            db.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id)
+                    VALUES ("${newEmploy.firstName}", "${newEmploy.lastName}", "${newEmploy.role}", "${newEmploy.manager}")`, (err, result) => {
+            if (err) {
+                console.log(err)
+            } else {
+                console.log(result)
+            }
+        })
     }
+    
     //update employee role
     if (viewAllDept.start === 'update an employee role') {
+        let update = {}
+        db.query(`SELECT CONCAT(employee.first_name, ' ', employee.last_name) AS employee FROM employee`, (err,row) => {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            let update = +{row};
+            console.log(update)
+            return update;
+        });
+        
+
+
+        // const updateEmployee = await inquirer.prompt({
+        //     type: 'list',
+        //        name: 'manager',
+        //        message: "Please select the manager of the employee you would like to add",
+        //        choices: ["Bob Johnson", "Mike Chan", "No manager"]
+        // })
     }
 }
 
